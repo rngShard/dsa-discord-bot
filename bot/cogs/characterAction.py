2d013bfd8dcb1cce8586ef478341af798e384fe3
Oliver Berg <oliver.daniel.berg@gmail.com>
2020-04-27 18:03:20 +0200

    externalized attributes, refactoring to add skills to the mix

--- bot/cogs/characterAction.py
+++ bot/cogs/characterAction.py
@@ -5,9 +5,14 @@ import os
 import yaml
 from .diceRoller import Dice
 
-EIGENSCHAFTEN = ['MU', 'KL', 'IN', 'CH', 'FF', 'GE', 'KO', 'KK']
 DIR_NAME = os.path.dirname(__file__)
 CHAR_DIR = os.path.join(DIR_NAME, '../../chars')
+ASSETS_DIR = os.path.join(DIR_NAME, '../../assets')
+
+CHECKS = {
+    'ATTRIBUTES': yaml.full_load(open(os.path.join(ASSETS_DIR, 'attributes.yaml'))),
+    'SKILLS': None
+}
 
 class CharacterAction(commands.Cog):
     def __init__(self, bot):
@@ -17,21 +22,21 @@ class CharacterAction(commands.Cog):
     async def check(self, ctx, check: str, char='CTX_AUTHOR_DISPLAY_NAME'):
         """Check Kopfwert / Talent for character"""
         check = check.upper()
+
+        char_obj = None
         if char == 'CTX_AUTHOR_DISPLAY_NAME':
             char = ctx.author.display_name
-
-        errors = []
-        if not check in EIGENSCHAFTEN:
-            errors.append(f'"{check}" is not a valid EIGENSCHAFT to check for.\nPlease use one of {EIGENSCHAFTEN} instead.')
         char_files = os.listdir(CHAR_DIR)
         if f'{char}.yaml' not in char_files:
-            errors.append(f'No character-file found for character "{char}". Did you provide a "{char}.yaml" file?')
-        if len(errors) > 0:
-            logging.warning(f'Invalid .check command, args: {check}, {char}')
-            await ctx.send(f'Errors: {errors}')
+            await ctx.send(f'Errors: No character-file found for character "{char}". Did you provide a "{char}.yaml" file?')
+            return
         else:
-            with open(os.path.join(CHAR_DIR, f'{char}.yaml')) as char_file:
-                char_obj = yaml.full_load(char_file)
-                eig_value = char_obj[check]
-                roll = Dice.roll_dX(20)
-                await ctx.send(f'<{char}> Checking {check} ({eig_value}): {roll}. {"Success!" if roll <= eig_value else "Failure..."}')
+            char_obj = yaml.full_load(open(os.path.join(CHAR_DIR, f'{char}.yaml')))
+        
+        if check in CHECKS['ATTRIBUTES']:
+            eig_value = char_obj[check]
+            roll = Dice.roll_dX(20)
+            await ctx.send(f'<{char}> Checking {check} ({eig_value}): {roll}. {"Success!" if roll <= eig_value else "Failure..."}')
+        else:
+            logging.warning(f'Invalid .check command, args: {check}, {char}')
+            await ctx.send(f'Error: "{check}" is not a valid check.')
