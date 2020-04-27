f80d95cf48337d82141242f87fe10051400f90a2
Oliver Berg <oliver.daniel.berg@gmail.com>
2020-04-27 19:13:14 +0200

    skeletton for checking skills

--- bot/cogs/characterAction.py
+++ bot/cogs/characterAction.py
@@ -11,7 +11,7 @@ ASSETS_DIR = os.path.join(DIR_NAME, '../../assets')
 
 CHECKS = {
     'ATTRIBUTES': yaml.full_load(open(os.path.join(ASSETS_DIR, 'attributes.yaml'))),
-    'SKILLS': None
+    'SKILLS': yaml.full_load(open(os.path.join(ASSETS_DIR, 'skills.yaml')))
 }
 
 class CharacterAction(commands.Cog):
@@ -32,11 +32,15 @@ class CharacterAction(commands.Cog):
             return
         else:
             char_obj = yaml.full_load(open(os.path.join(CHAR_DIR, f'{char}.yaml')))
-        
+            logging.info(f'.check {check} {char}')
+
         if check in CHECKS['ATTRIBUTES']:
             eig_value = char_obj[check]
             roll = Dice.roll_dX(20)
             await ctx.send(f'<{char}> Checking {check} ({eig_value}): {roll}. {"Success!" if roll <= eig_value else "Failure..."}')
+        elif check in CHECKS['SKILLS'].keys():
+            # TODO: implement 
+            await ctx.send(f'<{char}> Checking {check}: Todo')
         else:
             logging.warning(f'Invalid .check command, args: {check}, {char}')
             await ctx.send(f'Error: "{check}" is not a valid check.')
