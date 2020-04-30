1e2029fc1bb1b76e06edc8b674b7ba82318569f8
Oliver Berg <oliver.daniel.berg@gmail.com>
2020-04-30 23:26:29 +0200

    adding spell checks

--- bot/cogs/characterAction.py
+++ bot/cogs/characterAction.py
@@ -12,7 +12,8 @@ ASSETS_DIR = os.path.join(DIR_NAME, '../../assets')
 
 CHECKS = {
     'ATTRIBUTES': yaml.full_load(open(os.path.join(ASSETS_DIR, 'attributes.yaml'))),
-    'SKILLS': yaml.full_load(open(os.path.join(ASSETS_DIR, 'skills.yaml')))
+    'SKILLS': yaml.full_load(open(os.path.join(ASSETS_DIR, 'skills.yaml'))),
+    'SPELLS': yaml.full_load(open(os.path.join(ASSETS_DIR, 'spells.yaml')))
 }
 
 class SkillCheck:
@@ -108,6 +109,19 @@ class CharacterAction(commands.Cog):
 
             await ctx.send(f'<{char}> Checking {check} {skill["attrs"]}={attrs} ({skill_value}) {self.pre(difficulty)}{difficulty}:\n{comment}Rolling {rolls} >> {msg}\nResult = {self.pre(res)}{res}')
         
+        elif check.lower() in CHECKS['SPELLS'].keys():
+            skill = CHECKS['SPELLS'][check.lower()]
+            attrs = [char_obj[attr] for attr in skill['attrs']]
+            try:
+                skill_value = char_obj[check.lower()]
+            except KeyError:
+                await ctx.send(f'<{char}> Cannot check {check}, because you do not know that spell.')
+                return
+
+            rolls, res, msg = SkillCheck.checkSkill(attrs, skill_value, difficulty)
+
+            await ctx.send(f'<{char}> Checking {check} {skill["attrs"]}={attrs} ({skill_value}) {self.pre(difficulty)}{difficulty}:\nRolling {rolls} >> {msg}\nResult = {self.pre(res)}{res}')
+
         else:
             logging.warning(f'Invalid .check command, args: {check}, {char}')
             await ctx.send(f'Error: "{check}" is not a valid check.')
