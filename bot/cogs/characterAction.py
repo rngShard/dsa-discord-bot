3360cd40fc7fb108302dd1cf75325c71f592832b
rescuehero <alexfinke@gmx.de>
2020-05-04 02:21:46 +0200

    beautiful output message through embed instead of plain text and ML

--- bot/cogs/characterAction.py
+++ bot/cogs/characterAction.py
@@ -16,6 +16,7 @@ CHECKS = {
     'SPELLS': yaml.full_load(open(os.path.join(ASSETS_DIR, 'spells.yaml')))
 }
 
+
 class SkillCheck:
     @staticmethod
     def checkSkill(attrs: [int, int, int], skill_value: int, difficulty=0):
@@ -27,7 +28,7 @@ class SkillCheck:
         rolls = []
         for _ in range(3):
             rolls.append(Dice.roll_dX(20))
-        
+
         msg = ""
         if rolls.count(1) == 2:
             msg = "Critical success! (thanks to 2x1)"
@@ -48,6 +49,7 @@ class SkillCheck:
             else:
                 msg = "Success!"
         return rolls, min(e_value, skill_value), msg
+
     @staticmethod
     def getTrueBE(BE: int, eBE: str):
         if re.compile(r'x\d').match(eBE):
@@ -57,26 +59,42 @@ class SkillCheck:
             minus = int(eBE[1])
             return BE - minus
 
+
 class CharacterAction(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
     def pre(self, num: int):
         return "+" if num >= 0 else ""
-  
-    @commands.command()
-    async def c(self, ctx, check: str, difficulty = 0):
-        await self.check(ctx, check, difficulty, 'CTX_AUTHOR_DISPLAY_NAME')
 
     @commands.command()
-    async def check(self, ctx, check: str, difficulty = 0, char = 'CTX_AUTHOR_DISPLAY_NAME'):
+    async def c(self, ctx, check: str, difficulty=0):
+        await self.check(ctx, check, difficulty, 'CTX_AUTHOR_DISPLAY_NAME')
+
+    @commands.command(pass_contect=True)
+    async def check(self, ctx, check: str, difficulty=0, char='CTX_AUTHOR_DISPLAY_NAME'):
         """Check Kopfwert / Talent for character"""
         char_obj = None
+
         if char == 'CTX_AUTHOR_DISPLAY_NAME':
             char = ctx.author.display_name
+
+        # adding embed for beautiful text visualization
+        embed = discord.embeds.Embed(
+            colour=discord.colour.Colour.orange()
+        )
+        author = f'{char}'
+        embed.set_author(name=f'{check.upper()} ({author})')
+        # - for special icon needed: user_id of author of message --> find profilpic through user_id
+        # embed.set_thumbnail(url='')
+        # embed.set_image(url='')
+        # embed.set_footer(text='TESTING',icon_url='')
+
         char_files = os.listdir(CHAR_DIR)
         if f'{char}.yaml' not in char_files:
-            await ctx.send(f'Errors: No character-file found for character "{char}". Did you provide a "{char}.yaml" file?')
+            embed.add_field(name=f'Error',
+                            value=f'No character-file found for character "{char}". Did you provide a "{char}.yaml" file?')
+            await ctx.send(embed=embed)
             return
         else:
             char_obj = yaml.full_load(open(os.path.join(CHAR_DIR, f'{char}.yaml')))
@@ -87,8 +105,12 @@ class CharacterAction(commands.Cog):
             roll = Dice.roll_dX(20)
             res = attr_value + difficulty - roll
             msg = "Success!" if res >= 0 else "Failure..."
-            await ctx.send(f'<{char}> Checking {check.upper()} ({attr_value}) {self.pre(difficulty)}{difficulty}:\nRolling {roll} >> {msg}\nResult = {self.pre(res)}{res}')
-        
+            embed.add_field(name=f'{check.upper()}', value=f'{attr_value}', inline=True)
+            embed.add_field(name=f'Modifier', value=f'{difficulty}', inline=True)
+            embed.add_field(name='Roll:', value=f'{roll}', inline=False)
+            embed.add_field(name='Result:', value=f'**{res} >> {msg}**', inline=False)
+            await ctx.send(embed=embed)
+
         elif check.lower() in CHECKS['SKILLS'].keys():
             comment, true_BE = "", None
             skill = CHECKS['SKILLS'][check.lower()]
@@ -96,9 +118,10 @@ class CharacterAction(commands.Cog):
             try:
                 skill_value = char_obj[check.lower()]
             except KeyError:
-                await ctx.send(f'<{char}> Cannot check {check}, because you do not know that skill.')
+                embed.add_field(name=f'Error', value=f'Cannot check {check}, because you do not know that skill.')
+                await ctx.send(embed=embed)
                 return
-                
+
             try:
                 eBE = skill['eBE']
                 BE = char_obj['BE']
@@ -107,28 +130,42 @@ class CharacterAction(commands.Cog):
                 difficulty -= true_BE
                 comment = f'(info) BE-relevant skill, difficulty adjusted for BE={BE} & eBE={eBE}, {self.pre(old_difficulty)}{old_difficulty} >> {self.pre(difficulty)}{difficulty}\n'
             except KeyError:
-                pass    # no BE to be accounted for
-            
+                pass  # no BE to be accounted for
+
             rolls, res, msg = SkillCheck.checkSkill(attrs, skill_value, difficulty)
 
-            await ctx.send(f'<{char}> Checking {check} {skill["attrs"]}={attrs} ({skill_value}) {self.pre(difficulty)}{difficulty}:\n{comment}Rolling {rolls}\nResult = {self.pre(res)}{res} >> {msg}')
-        
+            embed.add_field(name=f'Value:', value=f'{skill_value}', inline=True, )
+            embed.add_field(name=f'Modifier:', value=f'{difficulty}\n{comment}', inline=True)
+            embed.add_field(name=f'Properties', value=f'{skill["attrs"]}', inline=False)
+            embed.add_field(name=f'Properties', value=f'{attrs}', inline=True)
+            embed.add_field(name='Rolls:', value=f'{rolls}', inline=True)
+            embed.add_field(name='Result:', value=f'**{res} >> {msg}**', inline=False)
+            await ctx.send(embed=embed)
+
         elif check.lower() in CHECKS['SPELLS'].keys():
             skill = CHECKS['SPELLS'][check.lower()]
             attrs = [char_obj[attr] for attr in skill['attrs']]
             try:
                 skill_value = char_obj[check.lower()]
             except KeyError:
-                await ctx.send(f'<{char}> Cannot check {check}, because you do not know that spell.')
+                embed.add_field(name=f'Error', value=f'Cannot check {check}, because you do not know that spell.')
+                await ctx.send(embed=embed)
                 return
 
             rolls, res, msg = SkillCheck.checkSkill(attrs, skill_value, difficulty)
 
-            await ctx.send(f'<{char}> Checking {check} {skill["attrs"]}={attrs} ({skill_value}) {self.pre(difficulty)}{difficulty}:\nRolling {rolls} >> {msg}\nResult = {self.pre(res)}{res}')
+            embed.add_field(name=f'Value:', value=f'{skill_value}', inline=True, )
+            embed.add_field(name=f'Modifier:', value=f'{difficulty}', inline=True)
+            embed.add_field(name=f'Properties', value=f'{skill["attrs"]}', inline=False)
+            embed.add_field(name=f'Properties', value=f'{attrs}', inline=True)
+            embed.add_field(name='Rolls:', value=f'{rolls}', inline=True)
+            embed.add_field(name='Result:', value=f'**{res} >> {msg}**', inline=False)
+            await ctx.send(embed=embed)
 
         else:
             logging.warning(f'Invalid .check command, args: {check}, {char}')
-            await ctx.send(f'Error: "{check}" is not a valid check.')
+            embed.add_field(name='Error:', value=f'"{check}" is not a valid check.', inline=False)
+            await ctx.send(embed=embed)
 
     @commands.command()
     async def reg(self, ctx):
@@ -148,39 +185,39 @@ class CharacterAction(commands.Cog):
         else:
             char_obj = yaml.full_load(open(os.path.join(CHAR_DIR, f'{char}.yaml')))
 
-        reg_asp=0
-        reg_le=0
-        reg_kap=0 # change to 1 if priest
+        reg_asp = 0
+        reg_le = 0
+        reg_kap = 0  # change to 1 if priest
 
-        #additional regeneration rules: WdS 161
+        # additional regeneration rules: WdS 161
 
         # lifereg
-        reg_le+=Dice.roll_dX(6) # regularreg
-        if Dice.roll_dX(20)<=char_obj['KO']: # check if KO-check is succesful
-            reg_le+=1
+        reg_le += Dice.roll_dX(6)  # regularreg
+        if Dice.roll_dX(20) <= char_obj['KO']:  # check if KO-check is succesful
+            reg_le += 1
         # add character SF's (healing I/II/III - bad_healing I/II/III)
 
-        #asp-reg
+        # asp-reg
         # if not masterreg:
-        reg_asp+=Dice.roll_dX(6)
-        if Dice.roll_dX(20)<=char_obj['IN']: # check if IN-check is succesful
-            reg_asp+=1
+        reg_asp += Dice.roll_dX(6)
+        if Dice.roll_dX(20) <= char_obj['IN']:  # check if IN-check is succesful
+            reg_asp += 1
         # add character SF's (reg I, reg II, masterreg, astral_reg I/II/III)
         # regreg + adv - disadv + SF_reg
         # else: change with masterregrules
 
-        #ka-reg
-        reg_kap+=1 # if priest
+        # ka-reg
+        reg_kap += 1  # if priest
 
-        author=char
+        author = char
         embed = discord.embeds.Embed(
-            colour = discord.colour.Colour.orange()
+            colour=discord.colour.Colour.orange()
         )
 
         embed.set_author(name=f'regeneration')
         embed.add_field(name=f'LE', value=f'{reg_le}')
         embed.add_field(name=f'AsP', value=f'{reg_asp}')
         embed.add_field(name=f'KaP', value=f'{reg_kap}')
-        await ctx.send(author,embed=embed)
+        await ctx.send(author, embed=embed)
         # plain-text without embed:
         # await ctx.send(f'<{char}>\nregenerated:\n-**LE: {reg_le}**\n-**AsP: {reg_asp}** (if mage)\n-**KaP: {reg_kap}** (if priest)\nREMEMBER: regeneration modifiers not implemented yet!')
